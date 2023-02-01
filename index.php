10892c236e91f521fde43a7c682fd97269c85151
azkshourav <shourav.kst.bd@gmail.com>
2023-02-02 00:43:52 +0600

    new line added

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 echo " Hello world";
 echo " Hello shourav";
 echo " hello Kushtia"
+echo " How to push git from vs code? fist comment will be = git add . second comment is : git commit -m" new line added" last line is git push";
 ?>
